3f1b4b0c73739bb8c0dfeb2bc4a19b75e1ff9de6
Chris Nelson <superchrisnelson@gmail.com>
2021-06-29 11:42:49 -0400

    embedded signing example

--- lib/docusign_elixir_sample_app.ex
+++ lib/docusign_elixir_sample_app.ex
@@ -7,8 +7,11 @@ defmodule DocusignElixirSampleApp do
 
   alias DocuSign.Model.EnvelopeDefinition
   alias DocuSign.Model.TemplateRole
+  alias DocuSign.Model.EnvelopeSummary
   alias DocuSign.Model.EnvelopeRecipientTabs
+  alias DocuSign.Model.RecipientViewRequest
   alias DocuSign.Model.Text
+  alias DocuSign.Model.{EnvelopeRecipients, Signer}
 
   @doc """
   Fetches all envelopes younger than 30 days.
@@ -69,21 +72,54 @@ defmodule DocusignElixirSampleApp do
 
   def sign_it() do
     envelope = %EnvelopeDefinition{
-      templateId: "8ef64464-3d2d-4b85-b642-a9b67799b5ea",
-      templateRoles: [%TemplateRole{
-        roleName: "Aide",
-        email: "superchrisnelson@gmail.com",
-        name: "Chris Nelson",
-        tabs: %EnvelopeRecipientTabs{
-          textTabs: [%Text{
-            tabLabel: "SSN",
-            value: "123-45-6789"
-          }]
+      templateId: "ffb607c1-1f02-4d42-8948-6259bf39fca7",
+      # recipients: %EnvelopeRecipients{
+      #   signers: [%Signer{
+      #     email: "superchrisnelson@gmail.com",
+      #     userId: "12345"
+      #   }]
+      # },
+      status: "sent",
+      templateRoles: [
+        %TemplateRole{
+          clientUserId: "12345",
+          roleName: "Client",
+          email: "superchrisnelson@gmail.com",
+          name: "Chris Nelson",
+          tabs: %EnvelopeRecipientTabs{
+            textTabs: [
+              %Text{
+                tabLabel: "birth_date",
+                value: "10/11/1972"
+              },
+              %Text{
+                tabLabel: "address",
+                value: "641 Evangeline Rd Cincinnati OH 45240"
+              }
+            ]
+          }
         }
-      }]
+      ]
     }
 
-    {:ok, stuff} = Api.Envelopes.envelopes_post_envelopes(connection(), account_id(), envelopeDefinition: envelope)
+    {:ok, %EnvelopeSummary{envelopeId: envelopeId}} =
+      Api.Envelopes.envelopes_post_envelopes(connection(), account_id(),
+        envelopeDefinition: envelope
+      )
+
+    view_req = %RecipientViewRequest{
+      email: "superchrisnelson@gmail.com",
+      userName: "Chris Nelson",
+      authenticationMethod: "password",
+      returnUrl: "https://hhaa-dev.home52.org",
+      clientUserId: "12345"
+    }
+
+    {:ok, stuff} =
+      Api.EnvelopeViews.views_post_envelope_recipient_view(connection(), account_id(), envelopeId,
+        recipientViewRequest: view_req
+      )
+
     IO.inspect(stuff)
   end
 
